0ed7d07568196632f39c61a99adb318414759e21
samimesfun <samiestiff79@gmail.com>
2023-09-19 10:16:09 -0400

    VARIABLES

---
+++ seccond project/src/variables/VARIABLE.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class VARIABLE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			    double radius;
+			    double area;
+			    
+			    // Assign a radius
+			    radius = 20;
+			    
+			    // Compute area
+			    area = radius * radius * 3.14159;
+			    
+			    // Display results
+			    System.out.println("The area for the circle of radius " +
+			      radius + " is " + area);
+			  }
+			
+
+	}
+
+
