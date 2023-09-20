5ae1a5774a297860441404187226aaee4da00423
samimesfun <samiestiff79@gmail.com>
2023-09-19 22:37:16 -0400

    about escape characters

---
+++ seccond project/src/variables/ESCAPECHARACTERS.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class ESCAPECHARACTERS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//THIS IS EXAMPLES ABOUT TAB
+		System.out.println("10\tten");
+		System.out.println("20\ttweny");
+		System.out.println("30\tthirty");
+		System.out.println("40\tfrty");
+		// this is examole of new line character 
+		System.out.println("10\nten");
+		
+
+	}
+
+}
