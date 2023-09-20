632e5c64ed07ae860cb80e2f6628b5a77f97680a
samimesfun <samiestiff79@gmail.com>
2023-09-20 01:04:09 -0400

    It's assingment of the lab

---
+++ seccond project/src/assignment/LogicalOperatorsDemo.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public class LogicalOperatorsDemo {
+
+	public static void main(String[] args) {
+		boolean x = true;
+		boolean y = false;
+		System.out.println("x & y : " + (x & y));
+		System.out.println("x && y : " + (x && y));
+		System.out.println("x | y : " + (x | y));
+		System.out.println("x || y: " + (x || y));
+		System.out.println("x ^ y : " + (x ^ y));
+		System.out.println("!x : " + (!x));
+
+	}
+
+}
