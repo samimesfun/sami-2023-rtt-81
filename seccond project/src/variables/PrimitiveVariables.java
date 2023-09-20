44707aad99afbb51aee8b0ac12f0676e6d21fc54
samimesfun <samiestiff79@gmail.com>
2023-09-19 22:38:13 -0400

    about primitive data type

---
+++ seccond project/src/variables/PrimitiveVariables.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class PrimitiveVariables {
+
+	public static void main(String[] args) {
+		String x = new String();
+		x.toLowerCase();
+		x.toUpperCase();
+		
+		int j = 4;
+		
+		Integer k = Integer.valueOf(5);
+
+		boolean tuesday = true;
+		Boolean wednesday = new Boolean(false);
+
+	}
+
+}
