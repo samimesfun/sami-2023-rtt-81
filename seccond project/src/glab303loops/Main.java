7d6f052303c061e30bdd960b86648644b1380068
samimesfun <samiestiff79@gmail.com>
2023-09-25 23:04:42 -0400

    Glab 303 loops

---
+++ seccond project/src/glab303loops/Main.java
@@ -0,0 +1,17 @@
+package glab303loops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		int n = 1000;
+		// for loop
+		for (int i = 1; i <= n; ++i) {
+			// body inside for loop
+			sum += i; // sum = sum + i
+
+		}
+		System.out.println("Sum = " + sum);
+	}
+}
---
+++ seccond project/src/glab303loops/package-info.java
@@ -0,0 +1,1 @@
+package glab303loops;
